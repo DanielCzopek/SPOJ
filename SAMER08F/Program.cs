618f9a66fba8cbc95d2d1098d57dd03d90fc2b5a
Daniel Czopek <danielczopek102@gmail.com>
2022-06-01 14:28:44 +0200

    Samer08f zaczęte

---
+++ SAMER08F/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Problem
+{
+    private const int limit = 100;
+   
+
+    static Problem()
+    {
+        int[] kąty = new int[limit + 1];
+        kąty[0] = 0;
+        kąty[1] = 1;
+        kąty[2] = 5;
+
+        for (int i = 3; i <= limit; ++i)
+        {
+        
+            kąty[i] += 1;
+
+            kąty[i] += 4 * (i - 1);
+
+            
+            kąty[i] += 4 * SumFromOneUntil(i - 2);
+
+           
+            kąty[i] += kąty[i - 2];
+        }
+
+      
+    }
+
+    private static int SumFromOneUntil(int n)
+        => n * (n + 1) / 2;
+
+    
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+    }
+}
